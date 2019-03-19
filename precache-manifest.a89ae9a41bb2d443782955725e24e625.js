a95ff3f57a5805a269d3a4948cb2cc9bb4702308
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-03-19 00:31:39 -0700

    Updates

---
+++ precache-manifest.a89ae9a41bb2d443782955725e24e625.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "7c463e01536da7026534",
+    "url": "/robofriends/static/js/main.7c463e01.chunk.js"
+  },
+  {
+    "revision": "d09b68367c47976d5c7b",
+    "url": "/robofriends/static/js/1.d09b6836.chunk.js"
+  },
+  {
+    "revision": "7c463e01536da7026534",
+    "url": "/robofriends/static/css/main.5e0a75d6.chunk.css"
+  },
+  {
+    "revision": "d09b68367c47976d5c7b",
+    "url": "/robofriends/static/css/1.3e914207.chunk.css"
+  },
+  {
+    "revision": "4fe56c954a8694e43aa4d48b96349164",
+    "url": "/robofriends/index.html"
+  }
+];
